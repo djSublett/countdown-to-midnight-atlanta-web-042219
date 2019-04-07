e73ff05799ab3388b9d4f6dc68edc129dcfd5e08
Dale Sublett <djsublett04@gmail.com>
2019-04-07 22:26:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown
   end
   return "HAPPY NEW YEAR!"
 end
+
+
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
