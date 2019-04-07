206c10ab22f6d31c9c44f4ee446f0828f780f36a
Dale Sublett <djsublett04@gmail.com>
2019-04-07 22:29:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,6 @@
 #write your code here
 
-def countdown(x)
-  while x > 0
-    puts "#{x} SECONDS(S)!" 
-    x -= 1
-  end
-  return "HAPPY NEW YEAR!"
-end
+
 
 def countdown_with_sleep(seconds)
   while seconds > 0
